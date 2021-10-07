3b9fb6ff435f9fd35a3f6ecd31576606825476ff
ordinelli <vn691714@dal.ca>
2021-10-07 11:36:15 -0300

    Added test classes and test case for checking if user is returned to login page on logout.

---
+++ CSCI3130Project/app/src/androidTest/java/com/example/csci3130project/EspressoTest.java
@@ -0,0 +1,57 @@
+package com.example.csci3130project;
+
+
+import android.content.Context;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+public class EspressoTest {
+
+    @Rule
+    public ActivityScenarioRule<BaseActivity> myRule = new ActivityScenarioRule<>(BaseActivity.class);
+    public IntentsTestRule<BaseActivity> myIntentRule = new IntentsTestRule<>(BaseActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("ca.dal.cs.csci3130.a2", appContext.getPackageName());
+    }
+
+    @BeforeClass
+    public static void setup() {
+        Intents.init();
+    }
+
+    @Test
+    public void checkIfReturnedToLogin() {
+
+        onView(withId(R.id.loginBtn)).perform(click());
+        intended(hasComponent(Logout.class.getName()));
+        onView(withId(R.id.logoutButton)).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        intended(hasComponent(BaseActivity.class.getName()));
+    }
+
+
+}
