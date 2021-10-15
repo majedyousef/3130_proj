022cdcd7f38d61b1bbcb545708da001f512d023c
elokdah <hs659383@dal.ca>
2021-10-15 03:03:49 -0300

    Working registration code

--- app/src/main/java/com/example/csci3130project/ui/register/RegisterActivity.java
+++ app/src/main/java/com/example/csci3130project/ui/register/RegisterActivity.java
@@ -1,14 +1,12 @@
 package com.example.csci3130project.ui.register;
 
 import com.example.csci3130project.DatabaseUser;
-import com.example.csci3130project.MainActivity;
 import com.example.csci3130project.R;
-import com.example.csci3130project.ui.register.RegFragment;
+import com.example.csci3130project.MainActivity;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,11 +15,11 @@ import android.widget.Toast;
 import com.example.csci3130project.User;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText fname, lname, editEmail, password, password2;
-    private Button regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EditText fname, lname, editEmail, password, password2;
+        Button regBtn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_reg);
 
@@ -32,25 +30,31 @@ public class RegisterActivity extends AppCompatActivity {
         password = findViewById(R.id.editRegPass);
         password2 = findViewById(R.id.editRegPassConfirm);
 
-        Toast.makeText(RegisterActivity.this, "hi",Toast.LENGTH_SHORT);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-        regBtn.setOnClickListener(v -> {
+                String message = "registered";
+                //toasting
+                Toast t = Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP, 0, 150);
+                t.show();
+                String email = editEmail.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                String pass2 = password2.getText().toString().trim();
+                String firstName = fname.getText().toString().trim();
+                String lastName = lname.getText().toString().trim();
+                User user = new User(firstName, lastName, email, "user", pass);
+                DatabaseUser db = new DatabaseUser();
 
-            String email = editEmail.getText().toString().trim();
-            String pass = password.getText().toString().trim();
-            String pass2 = password2.getText().toString().trim();
-            String firstName = fname.getText().toString().trim();
-            String lastName = lname.getText().toString().trim();
-            User user = new User(firstName, lastName, email, "user", pass);
-            DatabaseUser db = new DatabaseUser();
+                if (pass.equals(pass2)) {
+                    db.add(user).addOnSuccessListener(suc -> {
+                        Toast.makeText(RegisterActivity.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }).addOnFailureListener(fal -> {
+                        Toast.makeText(RegisterActivity.this, "Data Insertion failed", Toast.LENGTH_SHORT).show();
+                    });
+                }
 
-            if (pass.equals(pass2)) {
-                db.add(user).addOnSuccessListener(suc -> {
-                    Toast.makeText(RegisterActivity.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                }).addOnFailureListener(fal -> {
-                    Toast.makeText(RegisterActivity.this, "Data Insertion failed", Toast.LENGTH_SHORT).show();
-                });
             }
         });
     }
