411ee3bbb1910109bd093a932e2b919a9bd808cc
yousef <mj425927@dal.ca>
2021-10-24 16:46:57 -0300

    Added a logout button to the profile page design

--- app/src/main/java/com/example/csci3130project/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/example/csci3130project/ui/profile/ProfileActivity.java
@@ -1,33 +1,34 @@
 package com.example.csci3130project.ui.profile;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.csci3130project.DatabaseUser;
 import com.example.csci3130project.R;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 
 public class ProfileActivity extends AppCompatActivity {
-    private Button changePassword,transactionHistory,settings;
-    private TextView email,userName,fullName;
-    private FirebaseDatabase database;
-    private DatabaseReference databaseReference;
+    private Button changePassword,transactionHistory,settings,logoutProfile;
+    private TextView profileEmail,profileUserName,profileFullName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_profile);
 
-        email = findViewById(R.id.EmailText);
-        userName = findViewById(R.id.UserNameText);
-        fullName = findViewById(R.id.FullNameText);
+        profileEmail = findViewById(R.id.EmailText);
+        profileUserName = findViewById(R.id.UserNameText);
+        profileFullName = findViewById(R.id.FullNameText);
         changePassword = findViewById(R.id.changePassBtn);
         transactionHistory = findViewById(R.id.transactionHistoryButton);
-        settings = findViewById(R.id.settingsButton);
+        logoutProfile = findViewById((R.id.logOutButtonProfile));
+        settings = findViewById(R.id.logOutButtonProfile);
     }
 }
+
 
