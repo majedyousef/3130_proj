19db6a3973c2926a7845c129a0b33f46fdd476ea
ordinelli <vn691714@dal.ca>
2021-10-25 15:03:26 -0300

    Refactored search bar code into its own layout xml file. This allows for the search bar layout to simply be included into any activity

--- app/src/main/java/com/example/csci3130project/MainActivity.java
+++ app/src/main/java/com/example/csci3130project/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+        //Setting the search text inside of the search bar.
+        SearchView searchView = (SearchView) findViewById(R.id.searchfield);
+        searchView.setQueryHint("Search for an item!");
+
     }
+
+
+
 }
