dec492a3a2f2f5c234a4477d943670ce11e7b90d
Erin Schultz <er825179@dal.ca>
2021-10-26 13:42:58 -0300

    Erin S - Created initial Item class and related unit test file

---
+++ app/src/main/java/com/example/csci3130project/Item.java
@@ -0,0 +1,15 @@
+package com.example.csci3130project;
+
+/**
+ * The Item class is used to create Item objects for the application.
+ * These objects store information about goods that are posted for trading.
+ *
+ * @author Group 6, CSCI3130 F21
+ */
+
+public class Item {
+
+    public Item() {
+
+    }
+}
