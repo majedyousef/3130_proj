8bd925cf2ef0379eb47321de6b7c56e7965854d3
Vincent Ordinelli <vn691714@dal.ca>
2021-10-28 14:28:31 -0300

    Refactored the search activity code from the main activity into its own separate search activity. added a button 'search for items' onto the main activity which when clicked takes the user to the search page where they can find items.

--- app/src/main/java/com/example/csci3130project/MainActivity.java
+++ app/src/main/java/com/example/csci3130project/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.csci3130project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -26,10 +29,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    SearchView searchView;
-    ListView listView;
-    ArrayList<String> list;
-    ArrayAdapter<String > adapter;
 
 
     @Override
@@ -40,60 +39,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         //This is just a simple method that can be commented in or out based on if the search data and stuff needs to be there.
-        //Just for my testing so it isnt all piled here in the main onCreate method. The method can be moved around anytime
-        createSearchBar();
+        //Just for my testing so it isnt all piled here in the main onCreate method. The method can be moved around later
+        //createSearchBar();
 
-
-    }
-
-    private void createSearchBar() {
-        // Creating the searchview and listeview object by finding the searchview and listview from the uI.
-        searchView = (SearchView) findViewById(R.id.searchView);
-        listView = (ListView) findViewById(R.id.lv1);
-        //creating an array to store the name of items that are available (in the future maybe database title reference etc?) and adding some dummy data
-        list = new ArrayList<>();
-        list.add("Used Office Chair for trade");
-        list.add("New Set of Kitchenware");
-        list.add("Like New Airpods");
-        list.add("Used Gaming PC");
-        list.add("Yard Equipment");
-        list.add("School Supplies");
-        list.add("Non Perishable Foods");
-        list.add("Books");
-        list.add("Used Kids Clothes");
-        list.add("Test 1");
-        list.add("Test 2");
-        list.add("Test 3");
-        list.add("Test 4");
-        list.add("Test 5");
-        list.add("Test 6");
-
-
-        //Creating a adapter for the listview and a on query text listener that will listen to the changes in the text view
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
-        listView.setAdapter(adapter);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            //This method is performed when the user clicks the enter button and "submits" the text. this can be modified to do other things
+        Button login = (Button) findViewById(R.id.searchButton);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-
-                if (list.contains(query)) {
-                    adapter.getFilter().filter(query);
-                } else {
-                    Toast.makeText(MainActivity.this, "No Match found", Toast.LENGTH_SHORT).show();
-                }
-                return false;
-            }
-
-            //This is the method that takes in the text as the user is typing and calls the filter method to see if there are items available regarding what the user is typing.
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
+            public void onClick(View view) {
+                Intent openMainAct = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(openMainAct);
             }
         });
+
+
     }
+
 
 
 }
---
+++ app/src/main/java/com/example/csci3130project/SearchActivity.java
@@ -0,0 +1,112 @@
+package com.example.csci3130project;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.csci3130project.databinding.ActivityBaseBinding;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity {
+
+    SearchView searchView;
+    ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter<String > adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchactivity);
+        createSearchBar();
+
+
+    }
+
+    private void createSearchBar() {
+        // Creating the searchview and listeview object by finding the searchview and listview from the uI.
+        searchView = (SearchView) findViewById(R.id.searchView);
+        listView = (ListView) findViewById(R.id.lv1);
+        //creating an array to store the name of items that are available (in the future maybe database title reference etc?) and adding some dummy data
+        list = new ArrayList<>();
+        list.add("Used Office Chair for trade");
+        list.add("New Set of Kitchenware");
+        list.add("Like New Airpods");
+        list.add("Used Gaming PC");
+        list.add("Yard Equipment");
+        list.add("School Supplies");
+        list.add("Non Perishable Foods");
+        list.add("Books");
+        list.add("Used Kids Clothes");
+        list.add("Test 1");
+        list.add("Test 2");
+        list.add("Test 3");
+        list.add("Test 4");
+        list.add("Test 5");
+        list.add("Test 6");
+
+
+        //Creating a adapter for the listview and a on query text listener that will listen to the changes in the text view
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            //This method is performed when the user clicks the enter button and "submits" the text. this can be modified to do other things
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                if (list.contains(query)) {
+                    adapter.getFilter().filter(query);
+                } else {
+                    Toast.makeText(SearchActivity.this, "No Match found", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            //This is the method that takes in the text as the user is typing and calls the filter method to see if there are items available regarding what the user is typing.
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+}
