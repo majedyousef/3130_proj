0380edac17350f53a5df9e146e49681d6b445be1
Erin Schultz <er825179@dal.ca>
2021-11-01 15:17:06 -0300

    Erin S - Implemented basic code for saving favourite categories

--- app/src/androidTest/java/com/example/csci3130project/SetFavouriteTests.java
+++ app/src/androidTest/java/com/example/csci3130project/SetFavouriteTests.java
@@ -7,9 +7,17 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.junit.Assert.assertEquals;
+
+import android.app.Activity;
+import android.content.Context;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +25,19 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class SetFavouriteTests {
 
+    @Rule
     public ActivityScenarioRule<SetFavourites> myRule = new ActivityScenarioRule<>(SetFavourites.class);
 
     @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.csci3130project", appContext.getPackageName());
+    }
+
+    @Test
     public void checkIfFavouritesConfirmed() {
+        onView(withId(R.id.category1)).perform(click());
         onView(withId(R.id.confirmButton)).perform(click());
         onView(withId(R.id.statusLabel)).check(matches(withText("Favourites have been saved.")));
     }
--- app/src/main/java/com/example/csci3130project/SetFavourites.java
+++ app/src/main/java/com/example/csci3130project/SetFavourites.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,10 +21,43 @@ public class SetFavourites extends AppCompatActivity {
 
         Button confirmButton = findViewById(R.id.confirmButton);
 
+        // Retrieve category checkboxes
+        CheckBox category1 = findViewById(R.id.category1);
+        CheckBox category2 = findViewById(R.id.category2);
+        CheckBox category3 = findViewById(R.id.category3);
+        CheckBox category4 = findViewById(R.id.category4);
+        CheckBox category5 = findViewById(R.id.category5);
+        TextView statusLabel = findViewById(R.id.statusLabel);
+
         // Confirm the user's choice of favourite categories
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // Reset status message
+                String emptyStatus = "";
+                statusLabel.setText(emptyStatus);
+
+                // If a category is selected, add it to the favourites collection
+                if (category1.isChecked()) {
+                    favourites.add(category1.getText().toString());
+                }
+                if (category2.isChecked()) {
+                    favourites.add(category2.getText().toString());
+                }
+                if (category3.isChecked()) {
+                    favourites.add(category3.getText().toString());
+                }
+                if (category4.isChecked()) {
+                    favourites.add(category4.getText().toString());
+                }
+                if (category5.isChecked()) {
+                    favourites.add(category5.getText().toString());
+                }
+
+                // Confirm favourite category selections
+                String savedMessage = "Favourites have been saved.";
+                statusLabel.setText(savedMessage);
 
             }
         });
