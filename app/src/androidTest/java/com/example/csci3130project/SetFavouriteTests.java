b2cb542d96b351eb6ff208cc3dd3bc6bd00ee3cb
Erin Schultz <er825179@dal.ca>
2021-11-01 14:47:05 -0300

    Erin S - Wrote espresso test for saving favourites

--- app/src/androidTest/java/com/example/csci3130project/SetFavouriteTests.java
+++ app/src/androidTest/java/com/example/csci3130project/SetFavouriteTests.java
@@ -1,5 +1,12 @@
 package com.example.csci3130project;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -14,7 +21,8 @@ public class SetFavouriteTests {
 
     @Test
     public void checkIfFavouritesConfirmed() {
-
+        onView(withId(R.id.confirmButton)).perform(click());
+        onView(withId(R.id.statusLabel)).check(matches(withText("Favourites have been saved.")));
     }
 
 }
