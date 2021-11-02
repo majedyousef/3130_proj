58d1b8d80ffb4019ec3d240524f0612e80346113
Erin Schultz <er825179@dal.ca>
2021-11-02 16:05:54 -0300

    Erin S - Created class for adding favourites to database

---
+++ app/src/main/java/com/example/csci3130project/DatabaseFavourites.java
@@ -0,0 +1,11 @@
+package com.example.csci3130project;
+
+/**
+ * The DatabaseFavourites class is used to submit a User's
+ * favourite categories to the database.
+ *
+ * @author Group 6, CSCI3130 F21
+ */
+
+public class DatabaseFavourites {
+}
