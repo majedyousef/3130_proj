9143803a8acfefe0fe1e2cc84543a29bb46df518
Erin Schultz <er825179@dal.ca>
2021-11-07 15:57:14 -0400

    Erin S - Fixing merge conflicts

--- app/src/main/java/com/example/csci3130project/Item.java
+++ app/src/main/java/com/example/csci3130project/Item.java
@@ -15,27 +15,27 @@ public class Item {
     private int itemID;
     private int userID;
     private static int count = 0;
-<<<<<<< app/src/main/java/com/example/csci3130project/Item.java
     private int itemValue;
     private double longitude;
     private double latitude;
 
-    public Item(String name, String description, String category,int itemValue, double latitude, double longitude) {
-=======
-
     public Item(String name, String description, String category) {
->>>>>>> app/src/main/java/com/example/csci3130project/Item.java
         this.name = name;
         this.description = description;
         this.category = category;
         count++;
         itemID = count;
-<<<<<<< app/src/main/java/com/example/csci3130project/Item.java
+    }
+
+    public Item(String name, String description, String category, int itemValue, double latitude, double longitude) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        count++;
+        itemID = count;
         this.itemValue = itemValue;
         this.longitude = longitude;
         this.latitude = latitude;
-=======
->>>>>>> app/src/main/java/com/example/csci3130project/Item.java
     }
 
     /**
@@ -70,19 +70,30 @@ public class Item {
         return category;
     }
 
-<<<<<<< app/src/main/java/com/example/csci3130project/Item.java
+    /**
+     * A method for retrieving the value of an item
+     * @return a double containing the value of this item
+     */
     public double getItemValue(){
         return this.itemValue;
     }
+
+    /**
+     * A method for retrieving the longitude of an item
+     * @return a double containing the longitude of this item
+     */
     public double getLongitude(){
         return this.longitude;
     }
+
+    /**
+     * A method for retrieving the latitude of an item
+     * @return a double containing the latitude of this item
+     */
     public double getLatitude(){
         return latitude;
     }
 
-=======
->>>>>>> app/src/main/java/com/example/csci3130project/Item.java
     /**
      * A method for updating the item's name
      * @param name - String containing new item name
@@ -106,16 +117,28 @@ public class Item {
     public void setCategory(String category) {
         this.category = category;
     }
-<<<<<<< app/src/main/java/com/example/csci3130project/Item.java
+
+    /**
+     * A method for updating the item's value
+     * @param itemValue - int containing new item value
+     */
     public void setItemValue(int itemValue){
-        this.itemValue =itemValue;
+        this.itemValue = itemValue;
     }
+
+    /**
+     * A method for updating the item's longitude
+     * @param longitude - double containing new item longitude
+     */
     public void setLongitude(double longitude){
         this.longitude = longitude;
     }
+
+    /**
+     * A method for updating the item's latitude
+     * @param latitude - double containing new item latitude
+     */
     public void setLatitude(double latitude){
         this.latitude = latitude;
     }
-=======
->>>>>>> app/src/main/java/com/example/csci3130project/Item.java
 }
