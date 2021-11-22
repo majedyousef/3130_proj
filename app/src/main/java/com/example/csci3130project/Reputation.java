ac9734b1a790a5a30346ab1405ae86a04b6fb3ca
Erin Schultz <er825179@dal.ca>
2021-11-22 15:11:31 -0400

    Erin S - Added Reputation class and unit test file

---
+++ app/src/main/java/com/example/csci3130project/Reputation.java
@@ -0,0 +1,16 @@
+package com.example.csci3130project;
+
+/**
+ * The Reputation class is used to create Reputation objects for the application.
+ * These objects store ratings and reviews for a user.
+ *
+ * @author Group 6, CSCI3130 F21
+ */
+
+public class Reputation {
+
+    private double rating;
+
+    public Reputation() {
+    }
+}
