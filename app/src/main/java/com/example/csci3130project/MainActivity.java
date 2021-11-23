4a2b693cf259163d91460e1bec6a00461302911b
chui <bn249585@dal.ca>
2021-11-23 18:24:09 -0400

    Added some more of the new icons, as well as the main activity and the corresponding XML file.

---
+++ app/src/main/java/com/example/csci3130project/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.csci3130project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Intents
+
+        // To Profile
+        Button profile = (Button) findViewById(R.id.profBtn);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageView profileImage = (ImageView) findViewById(R.id.profImg);
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // To Map
+        Button map = (Button) findViewById(R.id.mapBtn);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageView mapImage = (ImageView) findViewById(R.id.mapImg);
+        mapImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // To Search
+        Button search = (Button) findViewById(R.id.searchBtn);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageView searchImage = (ImageView) findViewById(R.id.searchImg);
+        searchImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // To Upload
+        Button upload = (Button) findViewById(R.id.uploadBtn);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), UploadActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageView uploadImage = (ImageView) findViewById(R.id.itemBtn);
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), UploadActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    // Disable going back to login
+    @Override
+    public void onBackPressed() { }
+}
