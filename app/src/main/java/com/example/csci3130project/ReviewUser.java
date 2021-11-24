ed1ea31fe3f3b2927cf27ee98df3261654b77dc4
Erin Schultz <er825179@dal.ca>
2021-11-24 15:21:37 -0400

    Erin S - Implemented some review functionality

--- app/src/main/java/com/example/csci3130project/ReviewUser.java
+++ app/src/main/java/com/example/csci3130project/ReviewUser.java
@@ -3,6 +3,11 @@ package com.example.csci3130project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class ReviewUser extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class ReviewUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_user);
+
+        RatingBar stars = (RatingBar) findViewById(R.id.userStars);
+        EditText comment = (EditText) findViewById(R.id.commentText);
+        Button submit = (Button) findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Get user rating and comment
+                float rating = stars.getRating();
+                String commentText = comment.getText().toString().trim();
+
+                // If no rating is entered, prompt the user
+                if (rating == 0) {
+                    Toast.makeText(getApplicationContext(), "Please select a rating.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // add review to user's reputation
+                }
+            }
+        });
     }
 }
