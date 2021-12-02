c75d5b71fa36427233eaac4e9c4678b31b226059
chui <bn249585@dal.ca>
2021-12-02 19:38:22 -0400

    Received the intents and populated the trade page.

--- app/src/main/java/com/example/csci3130project/TradeActivity.java
+++ app/src/main/java/com/example/csci3130project/TradeActivity.java
@@ -1,8 +1,12 @@
 package com.example.csci3130project;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class TradeActivity extends AppCompatActivity {
 
@@ -13,9 +17,15 @@ public class TradeActivity extends AppCompatActivity {
 
         String userIDIntent = getIntent().getStringExtra("userID");
         String itemIDIntent = getIntent().getStringExtra("itemID");
-        String itemValueIntent = getIntent().getStringExtra("itemValue");
+        Integer itemValueIntent = getIntent().getIntExtra("itemValue", 0);
+        String itemValue = itemValueIntent.toString();
 
+        TextView tradePartner = findViewById(R.id.viewTradePartner);
+        TextView tradeItem = findViewById(R.id.viewTradeItem);
+        TextView tradeValue = findViewById(R.id.viewTradeValue);
 
-
+        tradePartner.setText(userIDIntent);
+        tradeItem.setText(itemIDIntent);
+        tradeValue.setText(itemValue);
     }
 }
