099e0525966e319c9d290ec4ac1689d649b4b134
chui <bn249585@dal.ca>
2021-12-03 15:06:08 -0400

    Designed basic view of activity received.

--- app/src/main/java/com/example/csci3130project/ReceivedTradeActivity.java
+++ app/src/main/java/com/example/csci3130project/ReceivedTradeActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ReceivedTradeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_trade);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase firebase = FirebaseDatabase.getInstance();
+        DatabaseReference db = firebase.getReference();
+
+        if (user != null){
+
+        }
     }
 }
