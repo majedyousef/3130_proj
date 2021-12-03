3a5295e473ed22c7d2674dd9bd60610d33198a15
yousef <mj425927@dal.ca>
2021-12-02 20:12:57 -0400

    created another test for the notifications page.

--- app/src/androidTest/java/com/example/csci3130project/NotificationEspressoTest.java
+++ app/src/androidTest/java/com/example/csci3130project/NotificationEspressoTest.java
@@ -13,6 +13,8 @@ import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import junit.framework.AssertionFailedError;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -32,68 +34,10 @@ public class NotificationEspressoTest {
     public void setUp() throws Exception {
     }
 
-    //Checks if we moved to change password
+    //Checks if the title of page is present
     @Test
-    public void checkIfMovedToChangePass() {
-        onView(withId(R.id.changePassBtn)).perform(click());
-        intended(hasComponent(ChangePasswordActivity.class.getName()));
+    public void checkIfTitleIsVisible() {
+        onView(withId(R.id.ItemsNearMeHeader)).check(matches(withText("Items Near You")));
     }
 
-    //Checks if we moved to transaction history
-    @Test
-    public void checkIfMovedToTransactionHistoryPage() {
-        onView(withId(R.id.transactionHistoryButton)).perform(click());
-        intended(hasComponent(TransactionHistory.class.getName()));
-    }
-
-    //Checks if the old password is invalid
-    @Test
-    public void checkIfOldPasswordIsInvalid() {
-        onView(withId(R.id.changePassBtn)).perform(click());
-        intended(hasComponent(ChangePasswordActivity.class.getName()));
-        onView(withId(R.id.oldPasswordText)).perform(typeText("1234567"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.newPasswordText)).perform(typeText("lol12345678"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.resetPassButton)).perform(click());
-        onView(withId(R.id.displayTextView)).check(matches(withText("Old Password is Invalid!")));
-    }
-
-    //Checks if the new password is invalid
-    @Test
-    public void checkIfNewPasswordIsInvalid() {
-        onView(withId(R.id.changePassBtn)).perform(click());
-        intended(hasComponent(ChangePasswordActivity.class.getName()));
-        onView(withId(R.id.oldPasswordText)).perform(typeText("12345678"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.newPasswordText)).perform(typeText("lol"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.resetPassButton)).perform(click());
-        onView(withId(R.id.displayTextView)).check(matches(withText("New Password is Invalid!")));
-    }
-
-    //test that checks if passwords are the same
-    @Test
-    public void checkIfPasswordsSame() {
-        onView(withId(R.id.changePassBtn)).perform(click());
-        intended(hasComponent(ChangePasswordActivity.class.getName()));
-        onView(withId(R.id.oldPasswordText)).perform(typeText("12345678"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.newPasswordText)).perform(typeText("12345678"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.resetPassButton)).perform(click());
-        onView(withId(R.id.displayTextView)).check(matches(withText("Passwords Cannot be the Same!")));
-    }
-
-    //test that checks if password updates
-    @Test
-    public void checkIfPasswordUpdates() {
-        onView(withId(R.id.changePassBtn)).perform(click());
-        intended(hasComponent(ChangePasswordActivity.class.getName()));
-        onView(withId(R.id.oldPasswordText)).perform(typeText("12345678"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.newPasswordText)).perform(typeText("123456789"),ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.resetPassButton)).perform(click());
-        onView(withId(R.id.displayTextView)).check(matches(withText("")));
-    }
-
-    //Checks if moved back to login if we click sign out
-    @Test
-    public void checkIfSignsOut() {
-        onView(withId(R.id.logOutButtonProfile)).perform(click());
-        intended(hasComponent(LoginActivity.class.getName()));
-    }
 }
