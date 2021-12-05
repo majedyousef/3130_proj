bd8a2fecf3a2e96bef26fcd5ae0cbcd3288aec47
chui <bn249585@dal.ca>
2021-12-05 14:37:08 -0400

    Added the intents via searchHomeActivity

--- app/src/main/java/com/example/csci3130project/searchHomeActivity.java
+++ app/src/main/java/com/example/csci3130project/searchHomeActivity.java
@@ -2,7 +2,13 @@ package com.example.csci3130project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
 
 public class searchHomeActivity extends AppCompatActivity {
 
@@ -10,5 +16,72 @@ public class searchHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_home);
+
+        Button confirmButton = findViewById(R.id.searchConfBtn);
+
+        // Retrieve category checkboxes
+        CheckBox category1 = findViewById(R.id.searchCategory1);
+        CheckBox category2 = findViewById(R.id.searchCategory2);
+        CheckBox category3 = findViewById(R.id.searchCategory3);
+        CheckBox category4 = findViewById(R.id.searchCategory4);
+        CheckBox category5 = findViewById(R.id.searchCategory5);
+        CheckBox category6 = findViewById(R.id.searchCategory6);
+        CheckBox category7 = findViewById(R.id.searchCategory7);
+
+        category1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("category", category1.getText().toString());
+            }
+        });
+
+        category2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("category", category2.getText().toString());
+            }
+        });
+
+        category3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("category", category3.getText().toString());
+            }
+        });
+
+        category4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("category", category4.getText().toString());
+            }
+        });
+
+        category5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("category", category5.getText().toString());
+            }
+        });
+
+        category6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("category", category6.getText().toString());
+            }
+        });
+
+        category7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("category", category7.getText().toString());
+            }
+        });
     }
 }
